9f8f60b6622206da2e5ba0fae156d7e8427866bf
Khan-HB <hafsah999@gmail.com>
2023-10-22 23:35:18 -0400

    finished adding form components, submit button, and styled the form.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ export default function Contact() {
                 <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                ADDRESS
+                Address:
               </h2>
               <p className="mt-1">
                 House 951 Abu-Bakr St. <br />
@@ -30,13 +30,13 @@ export default function Contact() {
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                EMAIL
+                E-mail:
               </h2>
               <a className="text-indigo-400 leading-relaxed">
                 hafsah999@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE
+                Phone:
               </h2>
               <p className="leading-relaxed">123-456-7890</p>
             </div>
@@ -45,50 +45,50 @@ export default function Contact() {
         <form
           netlify
           name="contact"
-          className="">
-          <h2 className="">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
           </h2>
-          <p className="">
-            Please feel free to contact me about anything.
+          <p className="leading-relaxed mb-5">
+            Please contact me if you have any questions.
           </p>
-          <div className=" ">
-            <label htmlFor="name" className="">
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
             </label>
             <input
               type="text"
               id="name"
               name="name"
-              className=""
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <div className="">
-            <label htmlFor="email" className="">
-              Email
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email:
             </label>
             <input
               type="email"
               id="email"
               name="email"
-              className=""
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="">
-              Message
+              className="leading-7 text-sm text-gray-400">
+              Message:
             </label>
             <textarea
               id="message"
               name="message"
-              className=""
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
           <button
             type="submit"
-            className="">
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
         </form>
