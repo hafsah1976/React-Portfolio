8315ab0309fb154c7550d0c037bdbd3c91741646
Khan-HB <hafsah999@gmail.com>
2023-10-22 23:08:32 -0400

    added styling to the Skills component.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,23 +4,23 @@ import { skills } from "../data"; //import projects data
 
 export default function Skills() {
   return (
-    <section id="skills" className="">
-      <div className="">
-        <div className="">
-          <ChipIcon className="" />
-          <h1 className="">
+    <section id="skills" className="bg-gray-900 text-white py-10">
+      <div className="container px-5 mx-auto">
+        <div className="text-center mb-20">
+          <ChipIcon className="w-10 inline-block mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills & Technologies
           </h1>
-          <p className="">
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
             Here's a list of skills and technologies that I've learned at GW Fullstack Web-Dev bootcamp.
           </p>
         </div>
-        <div className="">
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
-            <div key={skill} className="">
-              <div className="">
-                <BadgeCheckIcon className="" />
-                <span className="">
+            <div key={skill} className="p-2 sm:w-1/2 w-full">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span className="title-font font-medium text-white">
                   {skill}
                 </span>
               </div>
