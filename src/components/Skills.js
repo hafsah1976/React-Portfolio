a2f1b0fceff952a70c3e6ba449317862905c5dd9
Khan-HB <hafsah999@gmail.com>
2023-10-22 23:07:28 -0400

    coded the skills.js file.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,33 @@
-// src/components/Skills.js
+import React from "react";
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid"; //for badges and icons
+import { skills } from "../data"; //import projects data
 
-export default function Skills() {}
+export default function Skills() {
+  return (
+    <section id="skills" className="">
+      <div className="">
+        <div className="">
+          <ChipIcon className="" />
+          <h1 className="">
+            Skills & Technologies
+          </h1>
+          <p className="">
+            Here's a list of skills and technologies that I've learned at GW Fullstack Web-Dev bootcamp.
+          </p>
+        </div>
+        <div className="">
+          {skills.map((skill) => (
+            <div key={skill} className="">
+              <div className="">
+                <BadgeCheckIcon className="" />
+                <span className="">
+                  {skill}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
