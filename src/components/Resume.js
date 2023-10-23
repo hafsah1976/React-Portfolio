04cbf991d782fb467b3fa3168125a3ac68fcd690
Khan-HB <hafsah999@gmail.com>
2023-10-23 01:19:05 -0400

    added imports to the resume component and the export statement..

---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+// Import React for creating React components
+import React from "react";
+
+// Import myResume, which is a link to the PDF resume file
+import Resume from "public/assets/document/Hafsah Nasreen.pdf";
+
+// Import icons from various icon libraries, possibly for use in the component
+import { BsCloudDownload } from "react-icons/bs"; // Download cloud icon
+import { MdFoundation } from "react-icons/md"; // Foundation icon
+import { GrTechnology } from "react-icons/gr"; // Technology icon
+import { GrDocumentPerformance } from "react-icons/gr"; // Document performance icon
+
+// Create and export Resume component
+export default function Projects() {
+    return };
