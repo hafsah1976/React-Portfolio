340c00d3145cc221ab80c999780c77ef9df7be81
Khan-HB <hafsah999@gmail.com>
2023-10-22 23:14:06 -0400

    working on Contact form. embedding map using iframe tag.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,21 @@
 // src/components/Contact.js
 
-export default function Contact() {}
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section id="contact" className="">
+      <div className="">
+        <div className="">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className=""
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "" }}
+            src=""
+          />
+      
