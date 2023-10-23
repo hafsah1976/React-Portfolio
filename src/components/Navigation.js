9eefe17d9ad1980ab112858ba559a55320293a74
Khan-HB <hafsah999@gmail.com>
2023-10-23 00:31:05 -0400

    adding to the navigation component, about, projects and skills, and contact me form

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,35 @@
-// src/components/Navigation.js
+import React from "react";
+import { ArrowRightIcon } from "@heroicons/react/solid"; 
 
-export default function Navigation() {}
+export default function Navbar() {
+  return (
+    <header className="">
+      <div className="">
+        <a className="">
+          {/* Logo with a link to the "about" section */}
+          <a href="#about" className="">
+            Hafsah Khan
+          </a>
+        </a>
+        <nav className="">
+          {/* Navigation links */}
+          <a href="#projects" className="">
+            My Work
+          </a>
+          <a href="#skills" className="">
+            Skills
+          </a>
+          <a href="#" className="">
+            
+          </a>
+        </nav>
+        <a
+          href="#contact"
+          className="">
+          Contact Me
+          <ArrowRightIcon className="" />
+        </a>
+      </div>
+    </header>
+  );
+}
