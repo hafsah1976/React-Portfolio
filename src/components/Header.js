2f6cd47676e21a1a34cc643afde55ed46ddbe683
Khan-HB <hafsah999@gmail.com>
2023-10-23 02:04:08 -0400

    added image to assets. added image to the Header component. Header component is complete.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../Navigation";
+import img from "../../assets/images/hop.jpg"; // Correct the image path
 
 function Header(props) {
   const { currentTab, handleTabChange } = props;
@@ -8,18 +9,34 @@ function Header(props) {
     <div>
       {/* Header section */}
       <section>
-        <header className="">
+        <header className="flex items-center justify-between px-4 py-2 bg-blue-500">
           <div>
-            <h1 className="">
+            <h1 className="text-3xl font-bold text-white">
               Hafsah Nasreen's Portfolio
             </h1>
           </div>
           <div>
-            <Nav
-              currentTab={currentTab}
-              handleTabChange={handleTabChange}
-            ></Nav>
+            <Nav currentTab={currentTab} handleTabChange={handleTabChange} />
           </div>
         </header>
       </section>
 
+      {/* Hero section */}
+      <section className="bg-gray-200 p-4">
+        <div className="hero-cta text-center">
+          <img
+            src={img}
+            className="my-2 w-full rounded-lg"
+            alt="cover"
+          />
+          <h2 className="text-2xl font-bold">Welcome</h2>
+          <p className="text-gray-700">
+            Welcome to Hafsah Nasreen's portfolio page. I am a student at the George Washington University Fullstack Web Development Bootcamp. During this program, I have had the opportunity to work on a variety of assignments and projects, which you can explore here. This portfolio is built with React and showcases a small sample of the work I've completed over the course of the program.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
