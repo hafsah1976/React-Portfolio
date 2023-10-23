316c2597437102f1f4c34fb238776cba6e77ee51
Khan-HB <hafsah999@gmail.com>
2023-10-23 01:53:58 -0400

    imported props from navigation. added <Nav> to handleTabChange and show the currentTab.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,3 +4,22 @@ import Nav from "../Navigation";
 function Header(props) {
   const { currentTab, handleTabChange } = props;
 
+  return (
+    <div>
+      {/* Header section */}
+      <section>
+        <header className="">
+          <div>
+            <h1 className="">
+              Hafsah Nasreen's Portfolio
+            </h1>
+          </div>
+          <div>
+            <Nav
+              currentTab={currentTab}
+              handleTabChange={handleTabChange}
+            ></Nav>
+          </div>
+        </header>
+      </section>
+
