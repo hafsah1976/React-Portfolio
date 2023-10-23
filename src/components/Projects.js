b7103b5b565a097e29645264d0cb6c85dc4bedff
Khan-HB <hafsah999@gmail.com>
2023-10-22 21:48:00 -0400

    finished styling the projects component.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,42 +1,42 @@
 //import projects.js
 
 import React from "react";
-import { projects } from "../data"; // to get data from the projects
-import { CodeIcon } from "@heroicons/react/solid"; // to add icons
+import { projects } from "../data";//import projects data
+import { CodeIcon } from "@heroicons/react/solid"; //add code  icons/image
 
 export default function Projects() {
   return (
-    <section id="projects" className="">
-      <div className="">
-        <div className="">
-          <CodeIcon className="" />
-          <h1 className="">
+    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <div className="flex flex-col w-full mb-20">
+          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             See What I've built
           </h1>
-          <p className="">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             Here are some of the projects that I have worked on, you can check them out and see more details about each one on my repo.
           </p>
         </div>
-        <div className="">
+         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
             <a
               href={project.link}
               key={project.image}
-              className="">
-              <div className="">
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
                 <img
-                  alt=""
-                  className=""
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="">
-                  <h2 className="">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="">
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="">{project.description}</p>
+                  <p className="leading-relaxed">{project.description}</p>
                 </div>
               </div>
             </a>
@@ -45,4 +45,4 @@ export default function Projects() {
       </div>
     </section>
   );
-}
+};
