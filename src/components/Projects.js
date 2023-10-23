0db9374e163bc2fdcb2968115b258da13384fd88
Khan-HB <hafsah999@gmail.com>
2023-10-22 21:46:02 -0400

    finished the projects component. that will access projects from the data file. added array method map() to get to specific project.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,48 @@
 //import projects.js
 
 import React from "react";
-import { projects } from "../data";
-import { CodeIcon } from "@heroicons/react/solid";
+import { projects } from "../data"; // to get data from the projects
+import { CodeIcon } from "@heroicons/react/solid"; // to add icons
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-        <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+    <section id="projects" className="">
+      <div className="">
+        <div className="">
+          <CodeIcon className="" />
+          <h1 className="">
             See What I've built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="">
             Here are some of the projects that I have worked on, you can check them out and see more details about each one on my repo.
           </p>
         </div>
+        <div className="">
+          {projects.map((project) => (
+            <a
+              href={project.link}
+              key={project.image}
+              className="">
+              <div className="">
+                <img
+                  alt=""
+                  className=""
+                  src={project.image}
+                />
+                <div className="">
+                  <h2 className="">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="">
+                    {project.title}
+                  </h1>
+                  <p className="">{project.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
