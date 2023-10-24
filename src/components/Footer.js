a6ebe45b794d46a9445dd9b8d5c708761b0271b4
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-24 08:10:50 -0400

    replaced icons imports in footer, skills, and projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { VscGithubAlt } from "react-icons/vsc";
-import { AiOutlineLinkedin } from "react-icons/ai";
-import { FaStackOverflow } from "react-icons/fa";
-import { Slide } from "react-awesome-reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
+import { Fade } from "react-reveal";
 
 function Footer() {
   return (
@@ -10,27 +9,23 @@ function Footer() {
       <h2 className="text-center text-lg font-semibold">Made by Hafsah Nasreen</h2>
 
       <ul className="flex justify-center mt-2">
-        <Slide direction="up" delay={100} triggerOnce>
+        <Fade cascade triggerOnce>
           <li className="mr-4">
             <a href="https://github.com/hafsah1976" className="social-link">
-              <VscGithubAlt className="w-6 h-6" />
+              <FontAwesomeIcon icon={faGithub} className="w-6 h-6" />
             </a>
           </li>
-        </Slide>
-        <Slide direction="up" delay={300} triggerOnce>
           <li className="mr-4">
             <a href="https://www.linkedin.com/in/hafsah-bukhari-66b87654" className="social-link">
-              <AiOutlineLinkedin className="w-6 h-6" />
+              <FontAwesomeIcon icon={faLinkedin} className="w-6 h-6" />
             </a>
           </li>
-        </Slide>
-        <Slide direction="up" delay={500} triggerOnce>
           <li>
             <a href="https://stackoverflow.com/users/your-user-id" className="social-link">
-              <FaStackOverflow className="w-6 h-6" />
+              <FontAwesomeIcon icon={faStackOverflow} className="w-6 h-6" />
             </a>
           </li>
-        </Slide>
+        </Fade>
       </ul>
     </footer>
   );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,30 @@
-import React from "react";  // Import the React library to define React components.
-import { skills } from "../data";  // Import the 'skills' array from an external data source.
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";  // Import the FontAwesomeIcon component.
-import { faCheck } from "@fortawesome/free-solid-svg-icons";  // Import the 'faCheck' icon from FontAwesome.
-import { faHtml5, faCss3, faJs, faReact, faNode, faDatabase, faGripfire } from "@fortawesome/free-brands-svg-icons";  // Import various icons from FontAwesome.
-import './Skills.css';  // Import the CSS file for styling this component.
+import React from "react";
+import { skills } from "../data";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faDatabase, faLeaf } from "@fortawesome/free-solid-svg-icons";
+import { faHtml5, faCss3, faJs, faReact, faNode, faGripfire } from "@fortawesome/free-brands-svg-icons";
+import '../assets/Skills.css';
 
-// Define a functional React component named 'Skills'.
 export default function Skills() {
   return (
-    <section id="skills" className="bg-gray-900 text-white py-10">  {/* Section for displaying skills */}
-      <div className="container px-5 mx-auto">  {/* Container for centering content */}
-        <div className="text-center mb-20">  {/* Center-align the content */}
-          <FontAwesomeIcon icon={faGripfire} className="w-10 inline-block mb-4" />  {/* Display a FontAwesome icon */}
-          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills & Technologies  {/* Display a title */}
+    <section id="skills" className="bg-gray-900 text-white py-10 fadeInUp">
+      <div className="container px-5 mx-auto">
+        <div className="text-center mb-20">
+          <FontAwesomeIcon icon={faGripfire} className="w-10 inline-block mb-4" />
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4 fadeInUp">
+            Skills & Technologies
           </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto fadeInUp">
             Here's a list of skills and technologies that I've learned at GW Fullstack Web-Dev bootcamp.
           </p>
         </div>
-        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">  {/* Flex container for skills list */}
-          {skills.map((skill, index) => (  // Map through the 'skills' array and render each skill
-            <div key={index} className="p-2 sm:w-1/2 w-full">  {/* Individual skill item */}
-              <div className="bg-gray-800 rounded flex p-4 h-full items-center">  {/* Skill item container */}
-                {getSkillIcon(skill)}  {/* Display the skill icon */}
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {skills.map((skill, index) => (
+            <div key={index} className="p-2 sm:w-1/2 w-full skill-item scaleIn slideIn">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                {getSkillIcon(skill)}
                 <span className="title-font font-medium text-white">
-                  {skill}  {/* Display the skill name */}
+                  {skill}
                 </span>
               </div>
             </div>
@@ -37,10 +36,8 @@ export default function Skills() {
 }
 
 function getSkillIcon(skill) {
-  // Define the default icon properties
   const iconProps = { icon: faCheck, size: "2x" };
 
-  // Check the skill and return the corresponding FontAwesome icon
   switch (skill) {
     case "HTML5":
       return <FontAwesomeIcon {...iconProps} icon={faHtml5} />;
@@ -56,10 +53,10 @@ function getSkillIcon(skill) {
       return <FontAwesomeIcon {...iconProps} icon={faDatabase} />;
     case "GraphQL":
       return <FontAwesomeIcon {...iconProps} icon={faGripfire} />;
+      case "MongoDB":
+        return <FontAwesomeIcon {...iconProps} icon={[faDatabase,faLeaf]}/>;
     default:
-      // Log an error message if the skill is unsupported
       console.error(`Unsupported skill: ${skill}`);
-      // Return a default icon (a checkmark) for unsupported skills
-      return <FontAwesomeIcon {...iconProps} />;
+      return null;
   }
 }
